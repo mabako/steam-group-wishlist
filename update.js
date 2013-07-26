efb49d0b2e33c0c9cb30b7a30957bcdbc25062f2
Marcus Bauer <mabako@gmail.com>
2013-07-26 21:37:57 +0200

    fixed members call for groups with >1k members

--- update.js
+++ update.js
@@ -6,39 +6,41 @@ var base = require('./base')
 
 module.exports = {
   members: function(req, page) {
-    // numeric group id? they have different urls
-    var name = req.data.name;
-    var url = /^\d+$/.test(name) ? ('gid/' + name) : ('groups/' + name);
-    base.fetch('http://steamcommunity.com/' + url + '/memberslistxml/?xml=1&p=' + page, function(err, content) {
-      if(err) {
-        console.log('Member fetching error for ' + url + '\n' + err);
-        req.io.emit('err', err.toString());
-      } else {
-        xml2js(content, function(err, res) {
-          if(err || !res) {
-            console.log('Member xml2js error for ' + url + '\n' + err);
-            // Steam error page maybe.
-            $ = cheerio.load(content);
-            var message = $('h3').text();
-            console.log('> ' + message);
-            req.io.emit('err', message);
-            return;
-          } else {
-            res = res.memberList;
-
-            if(res.currentPage == 1) {
-              apps.title(req, res.groupDetails[0].groupName, req.data.index)
-            }
-            req.io.emit('m', res.members[0].steamID64);
-            if(parseInt(res.currentPage) < parseInt(res.totalPages)) {
-              this.members(req, page + 1);
+    (function rec(req, page){
+      // numeric group id? they have different urls
+      var name = req.data.name;
+      var url = /^\d+$/.test(name) ? ('gid/' + name) : ('groups/' + name);
+      base.fetch('http://steamcommunity.com/' + url + '/memberslistxml/?xml=1&p=' + page, function(err, content) {
+        if(err) {
+          console.log('Member fetching error for ' + url + '\n' + err);
+          req.io.emit('err', err.toString());
+        } else {
+          xml2js(content, function(err, res) {
+            if(err || !res) {
+              console.log('Member xml2js error for ' + url + '\n' + err);
+              // Steam error page maybe.
+              $ = cheerio.load(content);
+              var message = $('h3').text();
+              console.log('> ' + message);
+              req.io.emit('err', message);
+              return;
             } else {
-              req.io.emit('k');
+              res = res.memberList;
+
+              if(res.currentPage == 1) {
+                apps.title(req, res.groupDetails[0].groupName, req.data.index)
+              }
+              req.io.emit('m', res.members[0].steamID64);
+              if(parseInt(res.currentPage) < parseInt(res.totalPages)) {
+                rec(req, page + 1);
+              } else {
+                req.io.emit('k');
+              }
             }
-          }
-        });
-      }
-    });
+          });
+        }
+      });
+    })(req, page);
   },
   friends: function(req) {
     var id = req.data.name.substr(8);
