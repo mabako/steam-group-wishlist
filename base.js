60f4009b0e0fdf1f8734e06489871254a32a7f87
Kemal Fadillah <fadillzzz@gmail.com>
2013-08-17 23:45:15 +0700

    Fix duplicate groups showing but without name

--- base.js
+++ base.js
@@ -19,14 +19,17 @@ module.exports = {
       } else {
         $ = cheerio.load(content);
 
-        var name = $('h1').text();
-        var groups = [];
+        var name = $('h1').text(),
+            groups = {};
+
         $('a.linkTitle').each(function(i, elem) {
-          var obj = $(this);
-          var link = obj.attr('href');
+          var obj = $(this),
+              link = obj.attr('href');
+
           link = link.substr(link.lastIndexOf('/') + 1);
-          groups[groups.length] = {url: link, name: obj.text()};
+          groups[link] = {url: link, name: obj.text()};
         });
+
         func(null, name, groups);
       }
     });
